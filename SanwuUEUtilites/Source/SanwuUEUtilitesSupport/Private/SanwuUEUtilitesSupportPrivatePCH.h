ba3df093113f951a4fba6af790e438e445ca7163
Sanwu-three <xtygah14three@hotmail.com>
2016-02-02 15:54:58 +0800

    修改Support模块

--- SanwuUEUtilites/Source/SanwuUEUtilitesSupport/Private/SanwuUEUtilitesSupportPrivatePCH.h
+++ SanwuUEUtilites/Source/SanwuUEUtilitesSupport/Private/SanwuUEUtilitesSupportPrivatePCH.h
@@ -2,5 +2,4 @@
 
 #include "Engine.h"
 #include "SanwuUEUtilites.h"
-#include "SanwuUEUtilitesClasses.h"
 #include "BlueprintGraphDefinitions.h"
--- SanwuUEUtilites/Source/SanwuUEUtilitesSupport/SanwuUEUtilitesSupport.Build.cs
+++ SanwuUEUtilites/Source/SanwuUEUtilitesSupport/SanwuUEUtilitesSupport.Build.cs
@@ -5,7 +5,7 @@ public class SanwuUEUtilitesSupport : ModuleRules
     public SanwuUEUtilitesSupport(TargetInfo TargetInfo)
     {
         PrivateDependencyModuleNames.AddRange(new string[] {
-            "Core","CoreUObject","Engine","BlueprintGraph","SanwuUEUtilites"
+            "Core","CoreUObject","Engine","SanwuUEUtilites","BlueprintGraph"
         });
     }
 }
