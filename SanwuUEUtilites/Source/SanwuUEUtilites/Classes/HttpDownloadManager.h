01f0d6c4741c6e59406311797653621a8c1e23d7
Sanwu-three <xtygah14three@hotmail.com>
2016-04-15 02:55:37 +0800

    启动多进程下载，

--- SanwuUEUtilites/Source/SanwuUEUtilites/Classes/HttpDownloadManager.h
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/HttpDownloadManager.h
@@ -5,6 +5,8 @@
 /*
 下载文件到本地
 */
+class UHttpDownloader;
+
 using namespace UF;
 using namespace UP;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FDownloadFileDelegate, FString, DestinationPath, float, DownloadPercent);
@@ -27,7 +29,11 @@ public:
 	FDownloadFileDelegate OnProgress;
 
 	UHttpDownloadManager* ParentDownloader;
-
+public:
+	FString GetMissionURL();
+	FString GetMissonContent();
+	FString GetDestinationPath();
+	void HandlePieceDownload(int32 DownloadedByte);
 private:
 
 	void GetFileSize();
@@ -35,14 +41,13 @@ private:
 	FString url;//请求下载的文件的URL地址
 	FString fileName;//保存到本地的文件名
 	FString DirctoryPath;//保存路径（文件夹）
+	FString DestinationPath;//最终路径
 	//**************Parent************
 	int32 FileTotalSize = 0;//文件总大小
-	TArray<FString>MissionPool;
-	FString CurrentMisson;
-	
-	//**************Child**************
-	void HandChildMissonComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	int32 FileDownloadedSize = 0;
+	TArray<FString>MissionPool;//任务池
 
+	static UHttpDownloadManager* Manager;
 	//HTTP请求完成或中断
 	void HandleGetFileSize(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 
---
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/HttpDownloader.cpp
@@ -0,0 +1,47 @@
+#include "SanwuUEUtilitesPrivatePCH.h"
+#include "HttpDownloader.h"
+#include "Http.h"
+DEFINE_LOG_CATEGORY_STATIC(LogHttpDownloader, Warning, All);
+UHttpDownloader::UHttpDownloader(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+UHttpDownloader* UHttpDownloader::Init(UHttpDownloadManager* Manager,int32 ID)
+{
+	UHttpDownloader* Downloader = NewObject<UHttpDownloader>();
+	Downloader->ID = ID;
+	Downloader->MissonURL = Manager->GetMissionURL();
+	Downloader->Manager = Manager;
+	Downloader->DestinationPath = Manager->GetDestinationPath();
+	Downloader->FetchMission();
+	return Downloader;
+}
+void UHttpDownloader::FetchMission()
+{
+	MissonContent = Manager->GetMissonContent();
+	if (MissonContent.Len()>0)
+	{
+		TSharedRef<class IHttpRequest> HttpRequest = FHttpModule::Get().CreateRequest();
+		HttpRequest->OnProcessRequestComplete().BindUObject(this, &UHttpDownloader::HandleMissonComplete);
+		HttpRequest->SetURL(MissonURL);
+		HttpRequest->SetVerb(TEXT("GET"));
+		HttpRequest->SetHeader("Range", MissonContent);
+		HttpRequest->ProcessRequest();
+	}
+}
+void UHttpDownloader::HandleMissonComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	FString EndWritePointStr;
+	FString StartWritePointStr;
+	MissonContent.Split("-",&StartWritePointStr, &EndWritePointStr);
+	StartWritePoint=FCString::Atoi(*StartWritePointStr);
+	UE_LOG(LogHttpDownloader, Warning, TEXT("Mission %s Complete"), *MissonContent);
+	if (FFileHelper::SaveArrayToFile(HttpResponse->GetContent(), *DestinationPath, &IFileManager::Get(), (uint32)StartWritePoint)) {
+		Manager->HandlePieceDownload(HttpResponse->GetContentLength());
+		FetchMission();
+	}
+	else {
+		UE_LOG(LogHttpDownloader, Error, TEXT("Mission %s Write to file fail"), *MissonContent);
+	}
+	
+}
