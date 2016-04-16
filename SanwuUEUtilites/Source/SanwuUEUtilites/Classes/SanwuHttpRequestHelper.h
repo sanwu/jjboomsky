3226b1f6d4c699bf5a09c44fe10f28c232dc34bf
Sanwu-three <xtygah14three@hotmail.com>
2016-04-16 22:31:40 +0800

    增加Http请求功能。

---
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuHttpRequestHelper.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "IHttpRequest.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "SanwuHttpRequestHelper.generated.h"
+/*
+下载文件到本地
+*/
+
+using namespace UF;
+using namespace UP;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FSanwuHttpRequestDelegate, FString, Content, int32, StatusCode);
+
+UENUM()
+enum SanwuHttpRequestType
+{
+	GET,
+	POST
+};
+
+UCLASS()
+class USanwuHttpRequestHelper : public UBlueprintAsyncActionBase
+{
+	GENERATED_UCLASS_BODY()
+public:
+	UFUNCTION(BlueprintCallable,Category="Sanwu|Http", meta = (BlueprintInternalUseOnly = "true"))
+	static USanwuHttpRequestHelper* HttpRequestString(FString url,SanwuHttpRequestType Type,FString Content);
+
+	UPROPERTY(BlueprintAssignable)
+	FSanwuHttpRequestDelegate OnSuccess;
+
+	UPROPERTY(BlueprintAssignable)
+	FSanwuHttpRequestDelegate OnFail;
+
+public:
+
+private:
+	void Start(FString url, SanwuHttpRequestType Type, FString Content);
+	void HandleRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+};
