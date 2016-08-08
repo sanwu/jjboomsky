373bb4008af8305906e3a5c3a4278e508124f79b
Sanwu-three <xtygah14three@hotmail.com>
2016-08-08 23:01:16 +0800

    增加动态加载Png图片，转换为UTexture2D的函数

--- SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuStringUtilityes.cpp
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuStringUtilityes.cpp
@@ -2,6 +2,8 @@
 
 #include "SanwuUEUtilitesPrivatePCH.h"
 #include "SanwuStringUtilityes.h"
+#include "IImageWrapper.h"
+#include "IImageWrapperModule.h"
 TArray<FString> USanwuStringUtilityes::SplitStringToArray(FString SRC, FString Arg)
 {
 	TArray<FString>result;
@@ -24,4 +26,33 @@ TArray<FString> USanwuStringUtilityes::SplitStringToArray(FString SRC, FString A
 		}
 		return result;
 	}
+}
+class UTexture2D* USanwuStringUtilityes::GetTexture2DFromDiskFile(const FString& FilePath)
+{
+	TArray<uint8> RawFileData;
+	UTexture2D* MyTexture = NULL;
+	if (FFileHelper::LoadFileToArray(RawFileData, *FilePath /*"<path to file>"*/))
+	{
+		IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(FName("ImageWrapper"));
+		// Note: PNG format.  Other formats are supported
+		IImageWrapperPtr ImageWrapper = ImageWrapperModule.CreateImageWrapper(EImageFormat::PNG);
+		if (ImageWrapper.IsValid() && ImageWrapper->SetCompressed(RawFileData.GetData(), RawFileData.Num()))
+		{
+			const TArray<uint8>* UncompressedBGRA = NULL;
+			if (ImageWrapper->GetRaw(ERGBFormat::BGRA, 8, UncompressedBGRA))
+			{
+				// Create the UTexture for rendering
+				MyTexture = UTexture2D::CreateTransient(ImageWrapper->GetWidth(), ImageWrapper->GetHeight(), PF_B8G8R8A8);
+
+				// Fill in the source data from the file
+				void* TextureData = MyTexture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+				FMemory::Memcpy(TextureData, UncompressedBGRA->GetData(), UncompressedBGRA->Num());
+				MyTexture->PlatformData->Mips[0].BulkData.Unlock();
+
+				// Update the rendering resource from data.
+				MyTexture->UpdateResource();
+			}
+		}
+	}
+	return MyTexture;
 }
--- SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuStringUtilityes.h
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuStringUtilityes.h
@@ -16,4 +16,7 @@ public:
 	/**把String转化为Array，根据一个标志*/
 	UFUNCTION(BlueprintCallable, Category = "SanwuString")
 	static TArray<FString> SplitStringToArray(FString SRC, FString Arg);
+
+	UFUNCTION(BlueprintCallable, Category = "Sanwu|Utilityes")
+	static class UTexture2D* GetTexture2DFromDiskFile(const FString& FilePath);
 };
