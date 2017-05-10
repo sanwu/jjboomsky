37fb6060774b6c74d9a8017371a7ab2831e3237b
Sanwuthree <three@sanwu.org>
2017-05-10 18:35:03 +0800

    完成下载模块

--- SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuHttpDownloadManager.h
+++ SanwuUEUtilites/Source/SanwuUEUtilites/Classes/SanwuHttpDownloadManager.h
@@ -40,37 +40,18 @@ public:
 
 	UHttpDownloadManager* ParentDownloader;
 public:
-	FString GetMissionURL();
-	FString GetMissonContent();
-	FString GetDestinationPath();
-	void HandlePieceDownload(int32 DownloadedByte);
-	float DownloadPercent;
-	
-	void HandMissionPartComplete();
-	TArray<FString>MissionPool;//任务池
+
 private:
-
-	void GetFileSize();
-
+	void StartMission();
 	FString url;//请求下载的文件的URL地址
-	FString fileName;//保存到本地的文件名
-	FString DirctoryPath;//保存路径（文件夹）
 	FString DestinationPath;//最终路径
 	//**************Parent************
 	int32 FileTotalSize = 0;//文件总大小
 	int32 FileDownloadedSize = 0;
-	
 
-	static UHttpDownloadManager* Manager;
 	//HTTP请求完成或中断
-	void HandleGetFileSize(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	void HandleHttpCompelete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 	void HandleHttpProgress(FHttpRequestPtr req, int32 uploaded, int32 downloaded);
-	void UpdateTimer();
-	FTimerHandle TimerHandle;
 
-	UWorld* GameWorld;
-	FCriticalSection MissionPoolCriticalSection;
-	int32 MissionPartNum;
-	int32 MissionPartCompleteNum;
 	
 };
